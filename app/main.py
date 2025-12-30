2eab943e1a9513c56bfeaa657d37c6b8c41f8832
Melon <ribudidze@gmail.com>
2025-12-30 20:05:49 +0200

    resolved cache problem

--- app/main.py
+++ app/main.py
@@ -6,15 +6,16 @@ def cache(func: Callable) -> Callable:
     cache_store = {}
 
     @wraps(func)
-    def wrapper(*args, **kwargs) -> Any:
+    def wrapper(*args, **kwargs):
+        cache_key = (args, tuple(sorted(kwargs.items())))
 
-        if args in cache_store:
+        if cache_key in cache_store:
             print("Getting from cache")
-            return cache_store[args]
+            return cache_store[cache_key]
         else:
             print("Calculating new result")
             result = func(*args, **kwargs)
-            cache_store[args] = result
+            cache_store[cache_key] = result
             return result
 
     return wrapper
